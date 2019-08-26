21c2b722442b4705668d802676cbb44a852a91d3
Tyler Hunt <47220891+TyyHunt@users.noreply.github.com>
2019-08-26 14:25:26 -0600

    adding react dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :weapons
-  resources :races
-  resources :klasses
+  namespace :api do
+    resources :weapons
+    resources :races
+    resources :klasses
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
