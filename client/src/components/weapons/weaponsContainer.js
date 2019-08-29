aa74d18475078a3c2e536ada94aa52099b5f939a
Tyler Hunt <47220891+TyyHunt@users.noreply.github.com>
2019-08-29 17:35:48 -0600

    added some styling to weapons container

--- client/src/components/weapons/weaponsContainer.js
+++ client/src/components/weapons/weaponsContainer.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { Table } from 'react-bootstrap';
 
 export const WeaponsContainer = ({ weapons }) =>
   <div>
-    <h1>Weapons</h1>
-    {weapons.map(weapon =>
-    <div>
-      <h5>{weapon.name}</h5>
-      <h5>{weapon.damage}</h5>
-      <h5>{weapon.dmg_type}</h5>
-    </div>
-    )}
+    <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Weapon</th>
+          <th>Damage</th>
+          <th>Damage Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        {weapons.map(weapon =>
+        <tr>
+          <td>{weapon.id}</td>
+          <td>{weapon.name}</td>
+          <td>{weapon.damage}</td>
+          <td>{weapon.dmg_type}</td>
+        </tr>
+        )}
+      </tbody>
+    </Table>
   </div>;
 
 const mapStateToProps = ({ weapons }) => ({
- weapons,
+  weapons,
 });
 
 
 export default connect(mapStateToProps)(WeaponsContainer);
+
