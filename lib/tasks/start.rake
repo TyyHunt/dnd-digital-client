d90b660c8820abb547b37183d28a753ed23a7d12
Tyler Hunt <47220891+TyyHunt@users.noreply.github.com>
2019-10-17 12:04:07 -0600

    working through pushing production to heroku

--- lib/tasks/start.rake
+++ lib/tasks/start.rake
@@ -1,3 +1,21 @@
-task :start do
- exec 'foreman start -p 3000'
+#task :start do
+# exec 'foreman start -p 3000'
+
+#  desc 'Start production server'
+#   task :production do
+#     exec 'NPM_CONFIG_PRODUCTION=true npm run postinstall && foreman start'
+#   end
+#end
+
+namespace :start do
+ task :development do
+   exec 'foreman start -p 3000'
+ end
+
+ desc 'Start production server'
+ task :production do
+   exec 'NPM_CONFIG_PRODUCTION=true npm run postinstall && foreman start'
+ end
 end
+
+desc 'Start development server'
---
+++ public/precache-manifest.55daa0fd2a182ee420be51cedff469a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22067bbf9857c31bf18e8be693194926",
+    "url": "/index.html"
+  },
+  {
+    "revision": "288ba48aec32e2f0c7ab",
+    "url": "/static/css/main.1d1922bb.chunk.css"
+  },
+  {
+    "revision": "7e50de1e6a14cf4eb2bf",
+    "url": "/static/js/2.4afa38e4.chunk.js"
+  },
+  {
+    "revision": "288ba48aec32e2f0c7ab",
+    "url": "/static/js/main.0ca9ad6f.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
